a016531c6070795c0b0f90bc3fb2859442a3be37
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-11-30 15:14:40 -0800

    update startup.cs

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,57 +1,97 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
-namespace AnimalShelter.Solution
+namespace AnimalShelter
 {
-    public class Startup
+public class Startup
+{
+    public static string SecretKey = "mysupersecret_secretkey!123";
+    public static string Issuer = "infologs.in";
+    public static string Audience = "global";
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+    Configuration = configuration;
+    }
 
-        public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                        ValidateIssuer = true,
+                        ValidIssuer = Issuer,
+                        ValidateAudience = true,
+                        ValidAudience = Audience,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey)),
+                        };
+                        options.Events = new JwtBearerEvents()
+                        {
+                        OnAuthenticationFailed = context =>
+                    {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    context.Response.ContentType = context.Request.Headers["Accept"].ToString();
+                    string _Message = "Authentication token is invalid.";
+                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                    {
+                        //context.Response.Headers.Add("Token-Expired", "true");
+                        //OR
+                        _Message = "Token has expired.";
+                        return context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                        {
+                        StatusCode = (int)HttpStatusCode.Unauthorized,
+                        Message = _Message
+                        }));
+                    }
+                    return context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                    {
+                        StatusCode = (int)HttpStatusCode.Unauthorized,
+                        Message = _Message
+                    }));
+                      //return Task.CompletedTask;
+                    }
+                        };
+                    });
+    services.AddDbContext<AnimalShelterContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+    services.AddControllers();
+    }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+    if (env.IsDevelopment())
+    {
+    app.UseDeveloperExceptionPage();
+    }
 
-            app.UseRouting();
+      // app.UseHttpsRedirection();
+    app.UseRouting();
 
-            app.UseAuthorization();
+    app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
     }
 }
+}
+
